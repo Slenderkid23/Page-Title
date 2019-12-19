0ba4788078821d72a0d86e54eaf715aa11a87d12
Slenderkid23 <57138543+Slenderkid23@users.noreply.github.com>
2019-12-19 18:16:10 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,64 @@
+$(".Class1").dblclick(function() {
+    $(".Class2").show();
+    $(".Class1").hide();
+});
+$(".Class2").click(function() {
+    $(".Class3").show();
+    $(".Class2").hide();
+    alert("Click");
+});
+$(".Class3").dblclick(function() {
+    $(".Class3").hide();
+});
+$(".Class3").click(function() {
+    $(".Class4").show();
+    $(".Class3").hide();
+});
+$(".Class4").hover(function() {
+    $(".Class5").show();
+    $(".Class4").hide();
+});
+$(".Class5").mouseenter(function() {
+    $(".Class6").show();
+    $(".Class5").hide();
+});
+$(".Class6").click(function() {
+    $(".Class7").show();
+    $(".Class6").hide();
+});
+$(".Class7").click(function() {
+    $(".Class8").show();
+    $(".Class7").hide();
+});
+$(".Class8").click(function() {
+    $(".Class9").show();
+    $(".Class8").hide();
+});
+$(".Class9").click(function() {
+    $(".img").show();
+    $(".Class9").hide();
+});
+$(".img").click(function() {
+    $(".img2").show();
+    $(".img").hide();
+});
+$(".img2").click(function() {
+    $(".img3").show();
+    $(".img2").hide();
+});
+$(".img3").click(function() {
+    $(".Class10").show();
+    $(".img3").hide();
+});
+$(".Class10").click(function() {
+ (".class13").show();
+});
+$(".Class12").click(function() {
+    $(".Class13").show();
+    $(".Class12").hide();
+});
+$(".Class13").click(function() {
+    $(".Class14").show();
+    $(".Class13").hide();
+    alert("Sans stole the treasure");
+});
